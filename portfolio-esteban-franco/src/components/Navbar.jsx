94a3fb5af09dd3d64d7e80c6bb4dda502a66db1f
EstebanNeorenFranco <estebanneorenfranco@gmail.com>
2023-03-15 02:10:22 -0300

    Update Navbar.jsx
    Fixed responsive details

--- portfolio-esteban-franco/src/components/Navbar.jsx
+++ portfolio-esteban-franco/src/components/Navbar.jsx
@@ -32,13 +32,13 @@ const Navbar = () => {
   };
 
   return (
-    <div className='sticky top-0 flex justify-between items-center h-24 max-w-[1240px] mx-auto px-10'>
+    <div className='sticky top-0 flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 md:px-20'>
         <h1 className='cursor-pointer font-mono w-full font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600'>EstebanFranco.dev</h1>
         <ul className='hidden md:flex'>
-        <li className='p-4 cursor-pointer'>Bio</li>
-        <li className='p-4 cursor-pointer'>Projects</li>
-        <li className='p-4 cursor-pointer'>Contact</li>
-      </ul>
+          <li className='p-4 cursor-pointer'>Bio</li>
+          <li className='p-4 cursor-pointer'>Projects</li>
+          <li className='p-4 cursor-pointer'>Contact</li>
+        </ul>
       {/* BOTON DARK-MODE */}
       <button onClick={handleThemeSwitch} className="pl-2 pr-4">
                         <DarkModeIcon className="hover:fill-blue-600 dark:fill-white dark:hover:fill-blue-600 transition-all duration-75 hover:transition-all hover:duration-700"/>
@@ -47,7 +47,7 @@ const Navbar = () => {
           {nav ? <CloseIcon size={20} className="cursor-pointer hover:fill-gray-400 dark:fill-white dark:hover:fill-gray-400 transition-all duration-75 hover:transition-all hover:duration-700" /> : <MenuIcon size={20} className="cursor-pointer hover:fill-gray-400 dark:fill-white dark:hover:fill-gray-400 transition-all duration-75 hover:transition-all hover:duration-700" />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 backdrop-blur-xl ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-          <h2 className='font-mono w-full pt-10 font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 pl-8'>EstebanFranco.dev</h2>
+          <h2 className='font-mono w-full pt-10 font-extrabold text-transparent text-lg bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 pl-8'>EstebanFranco.dev</h2>
           <li className='p-4 cursor-pointer pt-10 border-b border-gray-600'>Home</li>
           <li className='p-4 cursor-pointer border-b border-gray-600'>Bio</li>
           <li className='p-4 cursor-pointer border-b border-gray-600'>Projects</li>
