f006c82dd9d52d738feae96535b444f2a91eb1eb
EstebanNeorenFranco <estebanneorenfranco@gmail.com>
2023-03-15 01:50:47 -0300

    Hero
    Create a Hero section with my data. New images added

--- portfolio-esteban-franco/src/App.js
+++ portfolio-esteban-franco/src/App.js
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className=" bg-white dark:bg-[#000300] dark:text-white">
+    <div className=" bg-white dark:bg-[#03001C] dark:text-white">
       <Navbar/>
       <Hero/>
     </div>
--- portfolio-esteban-franco/src/components/Hero.jsx
+++ portfolio-esteban-franco/src/components/Hero.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import esteban from '../images/esteban.jpg'
+import flag from '../images/argentina.png' 
+
 
 function Hero() {
   return (
-    <div className='px-14 py-14 flex '>
-      <div className=' mx-auto text-center '>
-        <img src={esteban} alt='Image not found' className='rounded-full h-44'/>
-        <p className='py-8 font-sans font-semibold text-lg'>Hi! 🖐🏻</p>
-        
+    
+    <div>
+      <div className='px-14 py-4 flex '>
+        <div className=' mx-auto text-center '>
+          <img src={esteban} alt='Image not found' className='rounded-full h-44'/>
+          <h4 className='py-4 font-sans font-semibold text-2xl'>Hi! 🖐🏻</h4>
+        </div>
       </div>
-      
+      <div className='mx-auto text-center px-8 md:px-54 lg:px-80'>
+        <div className='justify-center flex items-center space-x-2'>
+          <p className=' font-sans text-lg font-semibold'>💻 I'm Esteban a web developer from Argentina</p>
+          <img src={flag} alt='Image not found' className='h-5 w-5'/>
+        </div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, possimus voluptatem. Mollitia velit soluta quibusdam sit alias rem debitis eos ducimus numquam ipsam modi esse ullam, sed libero illum! Soluta?</p>
+      </div>
+ 
     </div>
   )
 }
