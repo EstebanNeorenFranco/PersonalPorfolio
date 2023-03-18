6a47e1d86fc2e51fd0eecf0cf0d9b0de7d0bc5b4
EstebanNeorenFranco <estebanneorenfranco@gmail.com>
2023-03-18 15:30:42 -0300

    Update Hero.jsx

--- portfolio-esteban-franco/src/components/Hero.jsx
+++ portfolio-esteban-franco/src/components/Hero.jsx
@@ -12,12 +12,12 @@ function Hero() {
           <h4 className='py-8 font-sans font-semibold text-2xl'>Hi! 🖐🏻</h4>
         </div>
       </div>
-      <div className='pt-3 mx-auto text-center px-8 md:px-54 lg:px-80'>
+      <div className='pt-3 mx-auto text-center px-6 md:px-54 lg:px-80'>
         <div className='justify-center flex items-center space-x-2'>
-          <p className=' font-sans font-semibold text-lg dark:text-gray-100'>💻 I'm Esteban, a programmer especialising in <span className=' font-bold dark:text-white'>Full Stack development</span></p>
+          <p className=' font-sans  font-semibold text-lg dark:text-gray-100'>💻 I'm Esteban, a programmer especialising in <span className=' font-bold dark:text-white'>Full Stack development</span></p>
         </div>
-          <p className='pt-2 p- text-gray-800 dark:text-gray-200'>Argentina, Ciudad Autonoma de Buenos Aires</p>
-          <div className='pt-36'>
+          <p className='pt-2 text-gray-800 dark:text-gray-200'>Argentina, Ciudad Autonoma de Buenos Aires</p>
+          <div className='sm:pt-36 pt-20'>
               <KeyboardArrowDownIcon className='animate-ping duration-1000'/>
           </div>
           </div>
