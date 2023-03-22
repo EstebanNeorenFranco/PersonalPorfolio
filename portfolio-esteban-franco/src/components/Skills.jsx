e542cac2ee32d0b5425c79db4e3087169b7f01d1
EstebanNeorenFranco <estebanneorenfranco@gmail.com>
2023-03-22 19:54:32 -0300

    New archives

--- portfolio-esteban-franco/src/components/Skills.jsx
+++ portfolio-esteban-franco/src/components/Skills.jsx
@@ -12,12 +12,28 @@ function Skills() {
       <div className='mx-auto px-8 md:px-54 lg:px-80 '>
         <h2 className='flex pl-4 text-center pb-10 pt-10 w-full h-full text-3xl font-bold font-sans text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 rounded'>Skills:</h2>
         <div className='justify-center hidden sm:flex space-x-12 pb-12 object-center'>
-            <div className='w-20'><img src={css} alt="css" className=' w-auto h-auto'/></div>
-            <div className='w-28'><img src={js} alt="js" className=' w-auto h-auto'/></div>
-            <div className=' w-28'><img src={react} alt="react" className=' w-auto h-auto'/></div>
-            <div className='w-28'><img src={tailwind} alt="tailwind" className=' w-auto h-auto'/></div>
-            <div className='w-24'><img src={python} alt="python" className=' w-auto h-auto'/></div>
-            <div className='w-24'><img src={mongoDB} alt="tensorflow" className=' w-auto h-auto'/></div>
+            <div className='w-20'>
+                <img src={css} alt="css" className=' w-auto h-auto'/>
+            </div>
+            <div className='w-28'>
+                <img src={js} alt="js" className=' w-auto h-auto'/>
+            </div>
+            
+            <div className=' w-28'>
+                <img src={react} alt="react" className=' w-auto h-auto'/>
+            </div>
+            
+            <div className='w-28'>
+                <img src={tailwind} alt="tailwind" className=' w-auto h-auto'/>
+            </div>
+            
+            <div className='w-24'>
+                <img src={python} alt="python" className=' w-auto h-auto'/>
+            </div>
+            
+            <div className='w-24'>
+                <img src={mongoDB} alt="tensorflow" className=' w-auto h-auto'/>
+            </div>
         </div>
         <div className='justify-center sm:hidden space-y-10 pb-12 object-center'>
             <div className='flex space-x-12 items-center justify-center'>
